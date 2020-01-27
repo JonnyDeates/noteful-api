7cbcbc2f5a3dce8019a9967f279aecc1d24cd47e
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-27 11:23:23 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,8 +17,7 @@ app.use(cors({
     // allow requests with no origin - like mobile apps, curl, postman
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not ' +
-          'allow access from the specified Origin.';
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
